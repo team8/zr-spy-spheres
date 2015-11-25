4971cf63dafec625cef398ce2ed7d4af180f55ff
Eric Liu <ericxliu@gmail.com>
2015-11-25 10:39:33 -0800

    began work

---
+++ alliance/today/data.cpp
@@ -0,0 +1,16 @@
+float fuel = 60;
+float energy = 50;
+float cameraOn;
+float score = 0;
+float timeToSwitch;
+int mirrorsHeld = 0;
+bool isRed;
+float data[6];
+
+float initData() {
+
+}
+
+void updateData() {
+
+}
