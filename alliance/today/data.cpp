dd18f668ac6f56ea5d5bfcd2f25817a6cbaeed5e
Eric Liu <ericxliu@gmail.com>
2015-11-25 11:43:26 -0800

    added some data

--- alliance/today/data.cpp
+++ alliance/today/data.cpp
@@ -1,11 +1,18 @@
+float myState[12];
+float theirState[12];
 float fuel;
+float memoryUsed;
+float memoryLeft;
 float energy;
 bool cameraOn;
 float score;
+float timeToSwitch;
+float leadingEdge;
+float trailingEdge;
 int mirrorsHeld;
 bool isRed;
+bool isFacingOther;
 float data[6];
-float my_state[12];
 float uploadDir[3];
 float itemLoc[9][3];
 
@@ -16,10 +23,6 @@ void initData() {
 	isRed = isSphereRed();
 
 }
-bool isSphereRed() {
-	if (my_state[0] >0) return false;
-	return true;
-}
 
 void setItemLocations() {
 	if(isRed()) {
@@ -27,11 +30,42 @@ void setItemLocations() {
 	}
 }
 
+void initData() {
+	api.getMyZRState(myState);
+	isRed = isRed();
+	fuel = 60;
+	energy = 5;
+	cameraOn = true;
+	score = 0;
+	mirrorsHeld = 0;
+}
+
 void updateData() {
+	api.getMyZRState(myState);
+	api.getOtherZRState(theirState);
 	energy = game.getEnergy();
 	fuel = game.getFuelRemaining();
 	cameraOn = game.isCameraOn();
-	api.getMyZRState(my_state);
 	mirrorsHeld = game.getNumMirrorsHeld();
+	leadingEdge = getLeading();
+	trailingEdge = getTrailing();
+	timeLeft = 180 - game.getCurrentTime();
+	memoryUsed = game.getMemoryFilled();
+	memoryLeft = game.getMemorySize() - game.getMemoryFilled();
+	isFacingOther = game.isFacingOther();
+}
 
+bool isRed() {
+	if(myState[0] < 0) {
+		return true;
+	}
+	else return false;
+}
+
+float getLeading() {
+	float leading = 1.05 + game.getLightInterfacePosition();
+}
+
+float getTrailing() {
+	float trailing = 0.05 + game.getLightInterfacePosition();
 }
