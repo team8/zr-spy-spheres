49b893ce79f8820cb2775a2b73bae2919bc1e7c7
Ryan Ehrlich <ryansehrlich@gmail.com>
2015-11-25 11:37:15 -0800

    Added Facing toward Earth

--- alliance/today/pictureactions.cpp
+++ alliance/today/pictureactions.cpp
@@ -8,9 +8,15 @@ void uploadPictureAction() {
 
 //writen by motion team
 void faceEarthAction() {
-
+	float facing_vec[3];
+	mathVecSubtract(facing_vec, zr_pos(uploadDir), zr_pos(my_state), 3);
+	mathVecNormalize(facing_vec, 3);
+	api.setAttitudeTarget(facing_vec);
 }
 
 void faceOtherAction() {
-
+	float facing_vec[3];
+	mathVecSubtract(facing_vec, zr_pos(other_pos), zr_pos(my_state), 3);
+	mathVecNormalize(facing_vec, 3);
+	api.setAttitudeTarget(facing_vec);
 }
