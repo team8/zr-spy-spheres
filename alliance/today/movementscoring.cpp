d90cf7cd090e417ba4744fa4c5dfa5b82c9b3802
Robbie S <robbie@selwynsoftware.com>
2015-11-25 10:10:48 -0800

    added the fram for movement

--- alliance/today/movementscoring.cpp
+++ alliance/today/movementscoring.cpp
@@ -1,3 +1,5 @@
+float scores_move[5];
+//In the order of the enum Movements
 enum Movements{
 	STAY = 0,
 	MOVE_UP = 1,
@@ -8,5 +10,48 @@ enum Movements{
 };
 
 Movements move = STAY;
+
+float scoreStay(){
+
+}
+
+float scoreMoveUp(){
+
+}
+
+float scoreMoveDarkFollow(){
+
+}
+
+float scoreMoveLightFollow(){
+
+}
+
+float scoreMoveItemUp(){
+
+}
+
+float scoreMoveItemDown(){
+
+}
+
+void updateScore(){
+	scores_move[0] = scoreStay();
+	scores_move[1] = scoreMoveUp();
+	scores_move[2] = scoreMoveDarkFollow();
+	scores_move[3] = scoreMoveLightFollow();
+	scores_move[4] = scoreMoveItemUp();
+	scores_move[5] = scoreMoveItemDown();
+}
+
+
+
+
+
+
+
+
+
+
 
 
