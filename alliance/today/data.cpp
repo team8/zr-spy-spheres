947f4878eea60897df2105e4b5f7dd5293bc9291
bdavies01 <bertdaviezz@gmail.com>
2015-11-25 11:56:01 -0800

    Added item locations.

--- alliance/today/data.cpp
+++ alliance/today/data.cpp
@@ -15,6 +15,9 @@ bool isFacingOther;
 float data[6];
 float uploadDir[3];
 float itemLoc[9][3];
+float mirror[3];
+float point1[3];
+float point2[3];
 
 void initData() {
 	uploadDir[0] = 0.0f;
@@ -25,8 +28,40 @@ void initData() {
 }
 
 void setItemLocations() {
-	if(isRed()) {
-
+	float testMirror[3];
+	game.getItemLoc(testMirror, 7);
+	float testMirror1[3];
+	game.getItemLoc(testMirror1, 8);
+	float testItem1[3];
+	game.getItemLoc(testItem1, 3);
+	float testItem2[3];
+	game.getItemLoc(testItem2, 4);
+	float testItem3[3];
+	game.getItemLoc(testItem3, 5);
+	float testItem4[3];
+	game.getItemLoc(testItem4, 6);
+	if(isRed) {
+		if(testMirror[0] <0){
+			mirror[3] = testMirror;
+		}
+		if(testItem1[0] < 0) {
+			point1= testItem1;
+			point2= testItem3;
+		} else if(testItem2[0] < 0) {
+			point1= testItem2;
+			point2= testItem4;
+		}
+	} else {
+		if(testMirror[0] >0){
+			mirror[3] = testMirror;
+		}
+		if(testItem1[0] > 0) {
+			point1= testItem1;
+			point2= testItem3;
+		} else if(testItem2[0] > 0) {
+			point1= testItem2;
+			point2= testItem4;
+		}
 	}
 }
 
